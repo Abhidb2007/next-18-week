598c4764e8d7985628e32dc928457534b583184f
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-16 19:54:37 +0530

    code

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,17 +1,27 @@
+"use client";
 import axios from "axios"
 import {useEffect, useState} from "react"
 export default function User(){
-    const [loading, setloading]=useState(false);
-    const[data,setData]=useState({});
+    const [loading, setloading]=useState(true);
+    const[data,setData]=useState();
     useEffect(()=>{
         axios.get("https://next-18-week.offline.kirattechnologies.workers.dev/api/v1/users/details")
         .then(response=>{
             setData(response.data);
-            
+
         })
 
     },[])
+    if(loading){
+        return<div>
+            loading...
+        </div>
+
+    }
     return<div>
         User page
+        {data.name}
+        {data.email}
+       
     </div>
 }
