55aa877f3ca19291315d00bbc1da1f8144ecc22e
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-17 23:02:39 +0530

    add code

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,27 +1,10 @@
-"use client";
-import axios from "axios"
-import {useEffect, useState} from "react"
-export default function User(){
-    const [loading, setloading]=useState(true);
-    const[data,setData]=useState();
-    useEffect(()=>{
-        axios.get("https://next-18-week.offline.kirattechnologies.workers.dev/api/v1/users/details")
-        .then(response=>{
-            setData(response.data);
-
-        })
-
-    },[])
-    if(loading){
-        return<div>
-            loading...
-        </div>
-
-    }
-    return<div>
+import axios from "axios";
+export default async function User(){
+    const res=await axios.get("https://next-18-week.offline.kirattechnologies.workers.dev/api/v1/users/details")
+      const data=res.data;
+      return<div>
         User page
         {data.name}
         {data.email}
-       
-    </div>
+      </div>
 }
