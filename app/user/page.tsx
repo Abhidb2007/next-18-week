261d8510f189ce86c9ea94f5c0cf7d409ac34159
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-17 23:30:02 +0530

    code

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,10 +1,23 @@
-import axios from "axios";
-export default async function User(){
-    const res=await axios.get("https://next-18-week.offline.kirattechnologies.workers.dev/api/v1/users/details")
-      const data=res.data;
-      return<div>
-        User page
-        {data.name}
-        {data.email}
-      </div>
+import axios from 'axios';
+import{useEffect,useState} from "react"
+export default function User(){
+  const[loading,setloading]=useState(true);
+  const[userData,setuserData]=useState();
+  useEffect(()=>{
+    axios.get("https://next-18-week.kirattechnologies.workers.dev/api/v1/user/details")
+    .then(response=>{
+      setuserData(response.data);
+      setloading(false);
+    })
+  },[])
+  if(loading){
+    return <div>Loading...</div>
+  }
+  return(
+    <div>
+      <h1>User Details</h1>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
