15096ab3958d88d4052ab7fab46248743e1c2896
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-18 08:23:26 +0530

    code

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,23 +1,13 @@
 import axios from 'axios';
 import{useEffect,useState} from "react"
-export default function User(){
-  const[loading,setloading]=useState(true);
-  const[userData,setuserData]=useState();
-  useEffect(()=>{
-    axios.get("https://next-18-week.kirattechnologies.workers.dev/api/v1/user/details")
-    .then(response=>{
-      setuserData(response.data);
-      setloading(false);
-    })
-  },[])
-  if(loading){
-    return <div>Loading...</div>
-  }
-  return(
-    <div>
-      <h1>User Details</h1>
-      <p>Name: {userData.name}</p>
-      <p>Email: {userData.email}</p>
-    </div>
-  );
+export default async function User(){
+  const response =await axios.get("")
+  await new Promise(r=>setTimeout(r,5000));
+  const data=response.data;
+  console.log("hi")
+  return <div>
+    User Page 
+    {data.name}
+    {data.email}
+  </div>
 }
