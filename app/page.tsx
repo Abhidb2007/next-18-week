360f422c86b643f06885f1e7aed3c9660ee74220
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-20 21:09:07 +0530

    code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
 export default function Home(){
   return(
-    <div>
-      hi yhrtr
+    <div className="text-lg w-screen h-screen flex items-center">
+      <div>
+        Todo application
+        <br/>
+        <Link className="text-md border m-2" href="/signin">Sign in to Todo App</Link>
+        <br/>
+        <Link className="text-md border m-2" href="/signup">Sign up to Todo App</Link>  
       </div>
-  );
+    </div>
+  )
 }
