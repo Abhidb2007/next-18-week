638b65029f91f0eb951594a6969004433ecd633b
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-26 23:04:47 +0530

    add code

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,22 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
       name: "Login with email",
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "abhi@gmail.com" },
-        password: { label: "Password", type: "password" },
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "abhi@gmail.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
       },
       async authorize(credentials, req) {
         const username = credentials?.username;
@@ -16,7 +25,7 @@ const handler = NextAuth({
         console.log("Username:", username);
         console.log("Password:", password);
 
-       
+        
         const user = {
           id: "1",
           name: "Abhi",
@@ -24,15 +33,26 @@ const handler = NextAuth({
           username: "abhi@gmail.com",
         };
 
-       
+        
         if (username === user.username && password === "123456") {
           
-          return user;
         }
 
-        
+      
         return null;
       },
+    }),
+
+  
+    GoogleProvider({
+      clientId: "asd",
+      clientSecret: "Asd",
+    }),
+
+    
+    GitHubProvider({
+      clientId: "asd",
+      clientSecret: "Asd",
     }),
   ],
 });
