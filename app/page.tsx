d798013fbe27d406955cbbf7a48b6884f50e5ec0
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-26 23:46:31 +0530

    code

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
-// app/page.tsx
-export default function Page() {
-  return <div>Home page</div>;
+"use client";
+import {SessionProvider,signOut, useSession,signIn} from "next-auth/react";
+export default function Home(){
+  return <SessionProvider>
+    <RealHome/>
+    </SessionProvider>
+}
+function RealHome(){
+  const session=useSession();
+  return(
+    <div>
+      {session.status=="authenticated"&&<button onClick={()=>signOut()}>Logout</button>}
+       {session.status=="unauthenticated"&&<button onClick={()=>signIn()}>Signin</button>}
+
+    </div>
+  )
 }
